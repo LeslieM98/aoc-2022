00679e71930cda804ac40881e7069481f4325510
Leslie <XXX>
2022-12-06 01:18:05 +0100

    Day05 part 1 done

--- src/day05.rs
+++ src/day05.rs
@@ -1,11 +1,70 @@
+#[derive(PartialEq, Debug)]
 struct Instruction {
     amount: usize,
     from: usize,
     to: usize,
 }
 
+impl Instruction {
+    pub fn parse(input: &String) -> Instruction {
+        let mut amount_str = String::new();
+        let mut from_str = String::new();
+        let mut to_str = String::new();
+        let mut status = 0;
+        for char in input.chars() {
+            if status == 0 {
+                if char == ' ' {
+                    status = 1;
+                }
+            } else if status == 1 {
+                if char == ' ' {
+                    status = 2;
+                } else {
+                    amount_str.push(char);
+                }
+            } else if status == 2 {
+                if char == ' ' {
+                    status = 3;
+                }
+            } else if status == 3 {
+                if char == ' ' {
+                    status = 4;
+                } else {
+                    from_str.push(char);
+                }
+            }else if status == 4 {
+                if char == ' ' {
+                    status = 5;
+                }
+            }else if status == 5 {
+                if char == '\n' {
+                    status = 6;
+                } else {
+                    to_str.push(char);
+                }
+            }
+        }
+
+        Instruction{
+            to: to_str.parse().unwrap(),
+            from: from_str.parse().unwrap(),
+            amount: amount_str.parse().unwrap()
+        }
+    }
+
+    pub fn execute(&self, stacks: &mut Vec<Vec<char>>) {
+        for _ in 0..self.amount {
+            let from = stacks.get_mut(self.from - 1).unwrap();
+            let tmp = from.pop().unwrap();
+            let to = stacks.get_mut(self.to - 1).unwrap();
+            to.push(tmp);
+        }
+    }
+}
+
+#[derive(PartialEq, Debug)]
 struct Warehouse {
-    stacks: Vec<Vec<char>>
+    stacks: Vec<Vec<char>>,
 }
 
 impl Warehouse {
@@ -17,7 +76,7 @@ impl Warehouse {
         return 1 + position * 4;
     }
 
-    pub fn parse_stacks(input: &Vec<String>) -> Vec<Vec<char>>{
+    pub fn parse(input: &Vec<String>) -> String{
         let mut stacks = vec![];
         let mut end_idx = 0;
         for line_idx in 0..input.len() {
@@ -35,7 +94,7 @@ impl Warehouse {
             }
         }
 
-        for line_idx in end_idx .. 0 {
+        for line_idx in (0..end_idx-1).rev() {
             let line = input.get(line_idx).unwrap();
             for i in 0..stacks.len() {
                 let position = Warehouse::get_horizontal_index_from_position(i);
@@ -49,9 +108,22 @@ impl Warehouse {
             }
         }
 
-        return stacks;
+        for line_idx in end_idx+1..input.len() {
+            let line = input.get(line_idx).unwrap();
+            let instruction = Instruction::parse(line);
+            instruction.execute(&mut stacks);
+        }
+
+        let mut solution = String::new();
+        for v in &mut stacks {
+            let tmp = v.pop().unwrap();
+            solution.push(tmp);
+        }
+        return solution;
     }
 }
+
+
 
 #[cfg(test)]
 mod tests {
@@ -66,15 +138,8 @@ mod tests {
     #[test]
     fn correct_stack_count() {
         let input = get_input();
-        let actual = Warehouse::parse_stacks(&input).len();
+        let actual = Warehouse::parse(&input).len();
         assert_eq!(3, actual);
-    }
-
-    #[test]
-    fn correct_stack_init() {
-        let input = get_input();
-        let actual = Warehouse::parse_stacks(&input);
-        assert_eq!(vec![vec!['Z', 'N'], vec!['M', 'C', 'D'], vec!['P']], actual)
     }
 
     #[test]
@@ -82,5 +147,26 @@ mod tests {
         assert_eq!(1, Warehouse::get_horizontal_index_from_position(0));
         assert_eq!(5, Warehouse::get_horizontal_index_from_position(1));
         assert_eq!(9, Warehouse::get_horizontal_index_from_position(2));
+    }
+    
+    #[test]
+    fn test_correct_instruction_parsing() {
+        let input = String::from("move 23 from 5 to 8\n");
+        let actual = Instruction::parse(&input);
+        assert_eq!(Instruction{from: 5, to: 8, amount: 23}, actual);
+    }
+
+    #[test]
+    fn generate_solution_1_test_input() {
+        let input = get_input();
+        let actual = Warehouse::parse(&input);
+        assert_eq!(String::from("CMZ"), actual);
+    }
+
+    #[test]
+    fn generate_solution_1() {
+        let input = util::get_input(5);
+        let actual = Warehouse::parse(&input);
+        assert_eq!(String::new(), actual);
     }
 }
